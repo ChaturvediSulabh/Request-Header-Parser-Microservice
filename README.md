9190fb0957a3cf2f222f047cc23ce3f3e1dfac1d
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2018-05-13 08:35:39 +0000

    Build 1.0 for Request Header API MS

---
+++ README.md
@@ -0,0 +1,3 @@
+			REQUEST HEADER PARSER MICROSERVICE
+
+User Story: I can get the IP address, language and operating system for my browser.
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const useragent = require('express-useragent');
+const request = require('request');
+app.enable('trust proxy');
+app.get('/', function(req, res){
+    res.sendFile('default.html', { root: __dirname + "/client"} );
+});
+app.get('/api/whoami',function(req,res,next){
+  let ip =req.ip;
+  ip = ip.replace(/[a-zA-Z:]+/g,'')
+  let sw = req.headers['user-agent'];
+  sw = sw.match(/\(.*\)/g)[0].replace(/[()]/g,'').split(';');
+  sw = sw[1]+' ; '+sw[2];
+  let lang = req.headers['accept-language'];
+  const myObj = {}
+  myObj.IPAddress = ip;
+  myObj.Language = lang;
+  myObj.Software = sw; 
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(myObj));
+  next();
+});
+app.listen(process.env.PORT || 8080);
